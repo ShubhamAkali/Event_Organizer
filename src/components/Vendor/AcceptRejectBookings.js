1bc993350e2dbadca78237042cce0a57abe55351
2795shital <shitalshelke58@gmail.com>
2024-08-10 17:24:16 -0700

    first commit

--- src/components/Vendor/AcceptRejectBookings.js
+++ src/components/Vendor/AcceptRejectBookings.js
@@ -5,6 +5,7 @@ const AcceptRejectBookings = () => {
     <div className="p-4">
       <h2 className="text-xl font-bold">Accept/Reject Bookings</h2>
       {/* Accept/Reject bookings content */}
+      
     </div>
   );
 };
